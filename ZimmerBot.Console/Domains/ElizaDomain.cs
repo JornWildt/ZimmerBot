da5753d4a68fda753c540c6f44784dee4d89f193
JornWildt <jw@fjeldgruppen.dk>
2016-03-15 16:12:44 +0100

    Playing more with Eliza.

--- ZimmerBot.Console/Domains/ElizaDomain.cs
+++ ZimmerBot.Console/Domains/ElizaDomain.cs
@@ -14,7 +14,19 @@ namespace ZimmerBot.Console.Domains
         .Response("Hvorfor? Gør <x> dig glad?");
 
       dd.AddRule(new RepitionWRegex(new WildcardWRegex()), new ChoiceWRegex(new WordWRegex("far"), new WordWRegex("mor"), "m"), new RepitionWRegex(new WildcardWRegex()))
-        .Response(i => ResponseHelper.OneOf(i, "Fortæl mig mere om din <m> ...", "Hvad var dit forhold til din <m>?"));
+        .Response(i => ResponseHelper.OneOf(i, 
+          "Fortæl mig mere om din <m> ...", 
+          "Hvad var dit forhold til din <m>?", 
+          "Hvad føler du i forhold til din <m>?",
+          "Hvordan forholder det sig til dine følelser i dag?",
+          "Ja, gode familierelationer er vigtige."));
+
+      dd.AddRule(new RepitionWRegex(new WildcardWRegex()), new ChoiceWRegex(new WordWRegex("barn"), new WordWRegex("barndom"), "m"), new RepitionWRegex(new WildcardWRegex()))
+        .Response(i => ResponseHelper.OneOf(i,
+          "Havde du nogle nære venner som barn?",
+          "Hvad er din ynglingserindring fra din barndom?",
+          "Blev du ind i mellem drillet af de andre børn?",
+          "Hvordan tror du at dine barndomsoplevelser har påvirket dig i dag?"));
     }
   }
 }
