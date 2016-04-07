4ccaeb1c1a77b2579c8123175156dfe517a7b06e
JornWildt <jw@fjeldgruppen.dk>
2016-04-07 08:51:24 +0200

    Verify ability to test (missing) values in output templates.

---
+++ ZimmerBot.Core.Tests/BotTests/OutputTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ZimmerBot.Core.ConfigParser;
+using ZimmerBot.Core.Knowledge;
+
+namespace ZimmerBot.Core.Tests.BotTests
+{
+  [TestFixture]
+  public class OutputTests : TestHelper
+  {
+    [Test]
+    public void CanTestForUndefinedValueInOutputTemplates()
+    {
+      BuildBot(@"
+> aaa
+: Value is <if(session.x)>SET<else>UNSET<endif>
+
+> bbb
+: OK
+! set session.x = 1
+");
+
+      AssertDialog("aaa", "Value is UNSET");
+      AssertDialog("bbb", "OK");
+      AssertDialog("aaa", "Value is SET");
+    }
+  }
+}
