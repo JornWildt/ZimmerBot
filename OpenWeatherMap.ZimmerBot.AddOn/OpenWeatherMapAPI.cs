18d657f5185dd1658e9b9cec99e804ab81dfe4a3
JornWildt <jw@elfisk.dk>
2019-03-05 16:45:30 +0100

    Working on the weather API

--- OpenWeatherMap.ZimmerBot.AddOn/OpenWeatherMapAPI.cs
+++ OpenWeatherMap.ZimmerBot.AddOn/OpenWeatherMapAPI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using log4net;
 using Ramone;
 using WebRequest = Ramone.Request;
@@ -27,36 +28,130 @@ namespace OpenWeatherMap.ZimmerBot.AddOn
     }
 
 
-    public Forecast GetForecast(string input)
+    public Forecast GetForecast(string input, int hours)
     {
       ISession session = NewSession();
 
-      WebRequest request = session.Bind("forecast?q={q}&units=metric&appid={key}", new { q = input, key = OpenWeatherMapAppSettings.Key.Value });
+      WebRequest request = session.Bind("forecast", new
+      {
+        q = input,
+        appid = OpenWeatherMapAppSettings.Key.Value,
+        units = "metric",
+        lang = "da",
+        mode = "xml",
+        cnt = (hours-1)/3 + 1 // One item for every 3 hours
+      });
 
-      using (var response = request.AcceptJson().Get<Forecast>())
+      using (var response = request.AcceptXml().Get<Forecast>())
       {
         return response.Body;
       }
     }
 
 
+    [XmlRoot("weatherdata")]
     public class Forecast
     {
-      public List<ForecastList> list { get; set; }
+      [XmlElement("sun")]
+      public ForecastSun Sun { get; set; }
+
+      [XmlArray("forecast")]
+      [XmlArrayItem("time")]
+      public List<ForecastItem> List { get; set; }
     }
 
 
-    public class ForecastList
+    public class ForecastSun
     {
-      public ForecastMain main { get; set; }
+      [XmlAttribute("rise")]
+      public DateTime Rise { get; set; }
+
+      [XmlAttribute("set")]
+      public DateTime Set { get; set; }
     }
 
 
-    public class ForecastMain
+    public class ForecastItem
     {
-      public decimal temp { get; set; }
-      public decimal temp_min { get; set; }
-      public decimal temp_max { get; set; }
+      [XmlAttribute("from")]
+      public DateTime From { get; set; }
+
+      [XmlAttribute("to")]
+      public DateTime To { get; set; }
+
+      [XmlElement("symbol")]
+      public ForecastSymbol Symbol { get; set; }
+
+      [XmlElement("windDirection")]
+      public ForecastWindDirection WindDirection { get; set; }
+
+      [XmlElement("windSpeed")]
+      public ForecastWindSpeed WindSpeed { get; set; }
+
+      [XmlElement("temperature")]
+      public ForecastTemperature Temperature { get; set; }
+
+      [XmlElement("clouds")]
+      public ForecastClouds Clouds { get; set; }
+    }
+
+
+    public class ForecastSymbol
+    {
+      [XmlAttribute("number")]
+      public string Number { get; set; }
+
+      [XmlAttribute("name")]
+      public string Name { get; set; }
+
+      [XmlAttribute("var")]
+      public string Var { get; set; }
+    }
+
+
+    public class ForecastWindDirection
+    {
+      [XmlAttribute("deg")]
+      public string Deg { get; set; }
+
+      [XmlAttribute("code")]
+      public string Code { get; set; }
+
+      [XmlAttribute("name")]
+      public string Name { get; set; }
+    }
+
+
+    public class ForecastWindSpeed
+    {
+      [XmlAttribute("mps")]
+      public decimal MetersPerSecond { get; set; }
+
+      [XmlAttribute("name")]
+      public string Name { get; set; }
+    }
+
+
+    public class ForecastTemperature
+    {
+      [XmlAttribute("value")]
+      public decimal Value { get; set; }
+
+      [XmlAttribute("min")]
+      public decimal Min { get; set; }
+
+      [XmlAttribute("max")]
+      public decimal Max { get; set; }
+    }
+
+
+    public class ForecastClouds
+    {
+      [XmlAttribute("value")]
+      public string Value { get; set; }
+
+      [XmlAttribute("all")]
+      public decimal all { get; set; }
     }
   }
 }
--- OpenWeatherMap.ZimmerBot.AddOn/OpenWeatherMapProcessor.cs
+++ OpenWeatherMap.ZimmerBot.AddOn/OpenWeatherMapProcessor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using log4net;
 using ZimmerBot.Core.Processors;
 
@@ -17,12 +19,85 @@ namespace OpenWeatherMap.ZimmerBot.AddOn
       var parameters = new Dictionary<string, object>();
 
       OpenWeatherMapAPI api = new OpenWeatherMapAPI();
-      OpenWeatherMapAPI.Forecast result = api.GetForecast(location);
+      OpenWeatherMapAPI.Forecast result = api.GetForecast(location, 48);
 
-      parameters["result"] = $"Temp: {result.list[0].main.temp}";
+      var restOfDay = ForecastRestOfDay(result);
+      var tomorrow = ForecastTomorrow(result);
+
+      parameters["today"] = restOfDay;
+      parameters["tomorrow"] = tomorrow;
 
       return new ProcessorOutput(parameters);
     }
 
+
+    private static Dictionary<string,string> ForecastRestOfDay(OpenWeatherMapAPI.Forecast forecast)
+    {
+      int firstTomorrowIndex = FindFirstTomorrow(forecast);
+
+      Dictionary<string, string> data = CalcForecastData(forecast, 0, firstTomorrowIndex);
+      data["period"] = "Resten af dagen";
+
+      return data;
+    }
+
+
+    private static Dictionary<string, string> ForecastTomorrow(OpenWeatherMapAPI.Forecast forecast)
+    {
+      int firstTomorrowIndex = FindFirstTomorrow(forecast);
+
+      Dictionary<string, string> data = CalcForecastData(forecast, firstTomorrowIndex, forecast.List.Count);
+      data["period"] = "I morgen";
+
+      return data;
+    }
+
+
+    private static int FindFirstTomorrow(OpenWeatherMapAPI.Forecast forecast)
+    {
+      int today = DateTime.Now.DayOfYear;
+      int firstTomorrowIndex = 0;
+
+      for (int i = 0; i < forecast.List.Count; ++i)
+      {
+        if (forecast.List[i].From.DayOfYear != today)
+        {
+          firstTomorrowIndex = i;
+          break;
+        }
+      }
+
+      return firstTomorrowIndex;
+    }
+
+
+    private static Dictionary<string, string> CalcForecastData(OpenWeatherMapAPI.Forecast forecast, int start, int end)
+    {
+      decimal minTemp = 100;
+      decimal maxTemp = -100;
+      Dictionary<string, int> weatherTypeCount = new Dictionary<string, int>();
+
+      for (int i = start; i<end; ++i)
+      {
+        var f = forecast.List[i];
+        if (f.Temperature.Min < minTemp)
+          minTemp = f.Temperature.Min;
+        if (f.Temperature.Max > maxTemp)
+          maxTemp = f.Temperature.Max;
+
+        if (!weatherTypeCount.ContainsKey(f.Symbol.Name))
+          weatherTypeCount[f.Symbol.Name] = 0;
+        weatherTypeCount[f.Symbol.Name]++;
+      }
+
+      var mostUsedWeatherType = weatherTypeCount.OrderBy(i => i.Value > i.Value).FirstOrDefault();
+
+      return new Dictionary<string, string>
+      {
+        ["minTemp"] = Math.Round(minTemp).ToString(),
+        ["maxTemp"] = Math.Round(maxTemp).ToString(),
+        ["weather"] = mostUsedWeatherType.Key
+      };
+    }
   }
 }
