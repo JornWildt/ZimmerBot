0976af44a95bebe2d5651ae535ca27070df9df18
JornWildt <jw@elfisk.dk>
2019-03-17 19:53:45 +0100

    Make matches serializable as we now store the previous result in the session.

--- ZimmerBot.Core/WordRegex/MatchResult.cs
+++ ZimmerBot.Core/WordRegex/MatchResult.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ZimmerBot.Core.Parser;
 
 namespace ZimmerBot.Core.WordRegex
 {
+  // Serializable since we store previoius matches in the session
+  [Serializable]
   public class MatchResult
   {
     public double Score { get; set; }
