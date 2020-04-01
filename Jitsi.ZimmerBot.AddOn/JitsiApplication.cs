ecc392d8951d2f86ec170aa5e32a04511d168e7b
JornWildt <jw@elfisk.dk>
2020-04-01 16:47:36 +0200

    Add Jitis meeting plugin.

---
+++ Jitsi.ZimmerBot.AddOn/JitsiApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZimmerBot.Core.AddOnHandling;
+using ZimmerBot.Core.Processors;
+
+namespace Jitsi.ZimmerBot.AddOn
+{
+  public class JitsiApplication : IZimmerBotAddOn
+  {
+    public void Initialize()
+    {
+      // Register processor functions available for use in scripts
+      ProcessorRegistry.RegisterProcessor("Jitsi.Meeting", JitsiProcessor.Meeting);
+    }
+
+
+    public void Shutdown()
+    {
+    }
+  }
+}
